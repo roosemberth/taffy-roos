b9d7e68fbb82ef345e9fe60799414d023993f469
Roosembert Palacios <roosembert.palacios@epfl.ch>
2019-10-24 17:07:01 +0200

    default.nix: Add librsvg Glib schemas
    This prevents crashing when using the mpris2New plugin and the mpris server did not
    send a thumbnail.
    
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- default.nix
+++ default.nix
@@ -1,2 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.haskellPackages.callCabal2nix "taffy-roos" ./. {}
+let
+  drv = pkgs.haskellPackages.callCabal2nix "taffy-roos" ./. {};
+in drv.overrideDerivation(self: {
+  nativeBuildInputs = self.nativeBuildInputs ++ [ pkgs.wrapGAppsHook ];
+  preFixup = ''
+    gappsWrapperArgs+=(
+      --prefix GDK_PIXBUF_MODULE_FILE : "$(echo ${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/*/loaders.cache)"
+    )
+  '';
+})
